000427826840b730dfa81ebad9db22bf5fb3acaa
Satwik <satwikmidya@gmail.com>
2025-06-16 18:05:22 +0530

    stated implementing the Algo

--- grid.py
+++ grid.py
@@ -13,6 +13,8 @@ class Cell:
         self.y=y
         self.w=w
         self.cell=[True,True,True,True]
+        self.visited = False
+        self.index= 0 
     
     def draw(self,canvas):
 
@@ -27,6 +29,22 @@ class Cell:
             cv.line(canvas,(x*w,(y+1)*w),((x+1)*w,(y+1)*w),(255,255,255),2)
         if(self.cell[3]):
             cv.line(canvas,((x+1)*w,y*w),((x+1)*w,(y+1)*w),(255,255,255),2)
+    
+
+    
+    def checkneighbours(self):
+        w = self.w
+        col = math.floor(500/w)
+        top = self.index - col
+        left = self.index -1
+        bottom = self.index + col
+        right = self.index +1
+        neighbours = [top,left,bottom,right]
+        return neighbours
+
+    
+        
+        
         
             
 
@@ -38,18 +56,32 @@ def setup(w):
     for i in range(row):
         for j in range(col):
             c=Cell(i,j,w)
+            c.index = i + j * col
             grid.append(c)
 
 
+def drawGrid():
+    setup(50)
+    for cell in grid:
+        cell.draw(blanck)
+
+
+
+def createmaze(row , col):
+    
+    row , col = row, col
+    current = Cell(row,col)
+    current.visited = True
+    
+    
 
 
 
 
-setup(50)
-for cell in grid:
-    cell.draw(blanck)
 
 
+
+drawGrid()
 
 cv.imshow('grid',blanck)
 cv.waitKey(0)
